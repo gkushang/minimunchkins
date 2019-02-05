e9e6cb8fcea6f5c7750dbd5ef8af0ecae4f5cbb9
gkushang <gkushang@gmail.com>
2019-02-05 10:42:13 -0500

    mini munckins

--- src/app/bottom-bar/bottom-bar.component.ts
+++ src/app/bottom-bar/bottom-bar.component.ts
@@ -12,4 +12,9 @@ export class BottomBarComponent implements OnInit {
   ngOnInit() {
   }
 
+  toTop() {
+     console.log('top =====');
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
 }
